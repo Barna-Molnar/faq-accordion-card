be1d291ed3e37dc5ce695872c4de35add42a9dd8
Barnabas Molnar <hanta911@gmail.com>
2021-06-12 17:29:08 +0200

    Animation with checkbox hack

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
-const questionBtn = document.querySelectorAll('.question__item__question')
-// const questionItem = document.querySelector('.question__item')
-// const answer = document.querySelectorAll('.question__item__answer')
-const arrow = document.querySelector('.arrow')
+// const questionBtn = document.querySelectorAll('.question__item__question')
+// // const questionItem = document.querySelector('.question__item')
+// // const answer = document.querySelectorAll('.question__item__answer')
+// const arrow = document.querySelector('.arrow')
 
 
 
 
-questionBtn.forEach((btn) => {
-    btn.addEventListener('click', function () {
+// questionBtn.forEach((btn) => {
+//     btn.addEventListener('click', function () {
 
-        const closestItem = btn.closest('.question__item')
-        const closestAnswer = document.querySelector('collapse')
-        console.log(closestAnswer)
-        btn.firstElementChild.classList.toggle('rotate')
-        closestItem.classList.toggle('height')
-        btn.classList.toggle('btnActive')
+//         const closestItem = btn.closest('.question__item')
+//         const closestAnswer = document.querySelector('collapse')
+//         console.log(closestAnswer)
+//         btn.firstElementChild.classList.toggle('rotate')
+//         closestItem.classList.toggle('height')
+//         btn.classList.toggle('btnActive')
 
-    })
-})
+//     })
+// })
 
 
